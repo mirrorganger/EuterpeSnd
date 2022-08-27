386bad9c17f3017a022b7845b1b7660491a72705
cesar <cesargufer@gmail.com>
2022-08-28 01:17:01 +0200

    Removed tests from build, added final steps to run them to github actions

--- cmake/TestingHelpers.cmake
+++ cmake/TestingHelpers.cmake
@@ -15,20 +15,8 @@ FUNCTION(addTest)
     target_link_libraries(${EXECUTABLE_TEST} gmock_main)
     include_directories(${gtest_SOURCE_DIR}/include)
 
-	set(unit_test_stamp ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE_NAME}_stamp)
-	add_custom_command(OUTPUT ${unit_test_stamp}
-		DEPENDS ${EXECUTABLE_TEST}
-		POST_BUILD
-		COMMAND ${EXECUTABLE_TEST} ${GTEST_COMMON_ARGS}
-		COMMAND ${CMAKE_COMMAND} -E touch ${unit_test_stamp}
-	)
-
     add_test(NAME ${EXECUTABLE_TEST} COMMAND ${EXECUTABLE_TEST})
 
-    add_custom_target(${EXECUTABLE_TEST}_run ALL DEPENDS ${unit_test_stamp} ${EXECUTABLE_TEST})
-    add_dependencies(COMPILE_TEST ${EXECUTABLE_TEST}_run)
-
-    
     include_directories({${LIBRARY_PATH}}src)
     target_link_libraries(${EXECUTABLE_TEST} ${LIBRARY_NAME})
 ENDFUNCTION()
