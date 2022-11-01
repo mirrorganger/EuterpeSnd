d22ea4099a52c089342b715657097b999d2611d7
cesar <cesargufer@gmail.com>
2022-11-01 23:43:32 +0100

    Added AudioRender interface

---
+++ synthtools/include/IAudioRender.h
@@ -0,0 +1,18 @@
+#ifndef EUTERPESND_IAUDIORENDER_H
+#define EUTERPESND_IAUDIORENDER_H
+
+#include <cstdint>
+
+namespace synthtools{
+/**
+ * Interface to be implemented by a device capable of produce audio.
+ */
+    class IAudioRender {
+    public:
+        virtual void renderAudio(float * audioData, uint32_t numFrames) = 0;
+        virtual ~IAudioRender() = default;
+    };
+
+}
+
+#endif //EUTERPESND_IAUDIORENDER_H
